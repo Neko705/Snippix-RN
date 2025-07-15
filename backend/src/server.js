7ffee4180d977d6bb871d35b99f4da8aaf8ba16b
Neko705 <129487693+Neko705@users.noreply.github.com>
2025-07-15 16:04:33 +0530

    vercel config added

--- backend/src/server.js
+++ backend/src/server.js
@@ -36,9 +36,11 @@ app.use((err, req, res, next) => {
 const startServer = async () => {
   try {
     await connectDB();
-    app.listen(ENV.PORT, () => {
-      console.log('Server is running on port: ', ENV.PORT);
-    });
+    if(ENV.NODE_ENV !== 'production') {
+      app.listen(ENV.PORT, () => {
+        console.log(`Server is running on port ${ENV.PORT}`);
+      });
+    }
   } catch (error) {
     console.error('Error starting server:', error);
     process.exit(1);
@@ -46,3 +48,6 @@ const startServer = async () => {
 };
 
 startServer();
+
+// Export the app for vercel and testing
+export default app;
